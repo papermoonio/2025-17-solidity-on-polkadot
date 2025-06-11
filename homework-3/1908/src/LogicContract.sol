de9fbf74411fbb6167c63c964ba9d6856a4fcd3f
ljjathena <128668121+ljjathena@users.noreply.github.com>
2025-06-12 00:56:22 +0800

    Update LogicContract.sol

--- homework-3/1908/src/LogicContract.sol
+++ homework-3/1908/src/LogicContract.sol
@@ -2,10 +2,9 @@
 pragma solidity ^0.8.0;
 
 contract LogicContract {
-    // 逻辑合约里必须有一个和代理合约一致的 storage 变量布局！
+    
     uint256 public num;
 
-    // 一个简单的函数，每次调用 num+1
     function addOne() public {
         num += 1;
     }
